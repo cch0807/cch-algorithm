8dfd8a79c794f50c98a2ad462690a36f110dd0ea
cch0807 <ckdgus11021@gmail.com>
2022-12-22 23:57:17 +0900

    20221222_2

---
+++ src/BOJ_2512.py
@@ -0,0 +1,14 @@
+# 입력
+n = int(input())
+requests = list(map(int, input().split()))
+budget = int(input())
+
+
+def calculate_needed_budget(upper_bound: int) -> int:
+    # 상한액이 upper_bound 일 때 필요한 예산을 계산하는 함수.
+    pass
+
+
+if __name__ == "__main__":
+    # 이분 탐색을 수행하는 메인 로직.
+    pass
