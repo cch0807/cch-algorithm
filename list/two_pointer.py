9e3c03d7791ecdabf4a40b9f550b3edc8cfcc3a5
choich0807 <ch.choi@aizen.co>
2023-03-24 17:20:41 +0900

    20230324_1

---
+++ list/two_pointer.py
@@ -0,0 +1,18 @@
+# Sort & Two Pointer
+# nums = {4,1,9,7,5,3,16}
+
+# nums.sort() # O(nlogn)
+
+def twoSum(nums, target) -> bool:
+    nums.sort()
+    l, r =0, len(nums)-1
+    while l<r:
+        if nums[l] + nums[r] > target:
+            r -= 1
+        elif nums[l] + nums[r] < target:
+            l += 1
+        elif nums[l] + nums[r] == target:
+            return True
+    return False
+
+print(twoSum(nums=[2,1,5,7], target=4))
