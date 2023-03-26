0ae36b4ebc1f5fc65a05377d69c6a67ae8acfc00
cch0807 <ckdgus11021@gmail.com>
2023-03-26 23:44:50 +0900

    20230326_1

---
+++ list/linked_list_2.py
@@ -0,0 +1,32 @@
+# Linked List의 다양한 활용
+# 1. Linked List 자유자재로 구현 (선형 자료구조 + 중간에 데이터 추가/삭제 용이)
+# 2. Tree or Graph에 활용
+
+# 인터넷 브라우저에서 방문기록과 동일한 작동을 하는 BrowserHistory class를 구현.
+# 구현할 브라우저는 homepage에서 시작하고, 이후에는 다른 url에 방문할 수 있다.
+# 또, "뒤로가기"와 "앞으로 가기"가 작동하도록 구현.
+
+
+class BrowserHistory(object):
+    def __init__(self, homepage):
+        """
+        : type homepage: str
+        """
+
+    def visit(self, url):
+        """
+        : type url: str
+        : rtype: None
+        """
+
+    def back(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
+
+    def forward(self, steps):
+        """
+        :type steps: int
+        :rtype: str
+        """
