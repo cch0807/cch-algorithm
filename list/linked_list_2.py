5f4584afc5bad5ce2f191eb8cedb0130c4fa9f6a
choich0807 <ch.choi@aizen.co>
2023-03-27 22:46:53 +0900

    20230327_1

--- list/linked_list_2.py
+++ list/linked_list_2.py
@@ -6,27 +6,41 @@
 # 구현할 브라우저는 homepage에서 시작하고, 이후에는 다른 url에 방문할 수 있다.
 # 또, "뒤로가기"와 "앞으로 가기"가 작동하도록 구현.
 
+class ListNode(object):
+    def __init__(self, val=0, next=None, prev=None):
+        self.val = val
+        self.next = next
+        self.prev = prev
 
 class BrowserHistory(object):
-    def __init__(self, homepage):
-        """
-        : type homepage: str
-        """
+    def __init__(self, homepage: str):
+        self.head = self.current = ListNode(val=homepage)
 
-    def visit(self, url):
-        """
-        : type url: str
-        : rtype: None
-        """
+    def visit(self, url: str):
+        self.current.next = ListNode(val=url, prev=self.current)
+        self.current = self.current.next
 
-    def back(self, steps):
-        """
-        :type steps: int
-        :rtype: str
-        """
+    def back(self, steps: int):
+        while steps > 0 and self.current.prev != None:
+            steps -= 1
+            self.current = self.current.prev
+        return self.current.val
 
-    def forward(self, steps):
-        """
-        :type steps: int
-        :rtype: str
-        """
+    def forward(self, steps: int):
+        while steps > 0 and self.current.next != None:
+            steps -= 1
+            self.current = self.current.next
+        return self.currnet.val
+    
+
+browserHistory = BrowserHistory("leetcode.com")
+browserHistory.visit("google.com")
+browserHistory.visit("facebook.com")
+browserHistory.visit("youtube.com")
+browserHistory.back(1)
+browserHistory.back(1)
+browserHistory.forward(1)
+browserHistory.visit("linkedin.com")
+browserHistory.forward(2)
+browserHistory.back(2)
+browserHistory.back(7)
---
+++ list/list_2.py
@@ -0,0 +1,23 @@
+# Array list 를 활용한 문제풀이
+# 인터넷 브라우저에서 방문기록과 동일한 작동을 하는 BrowserHistory class를 구현.
+# 구현할 브라우저는 homepage에서 시작하고, 이후에는 다른 url에 방문할 수 있다.
+# 또, "뒤로가기"와 "앞으로 가기"가 작동하도록 구현.
+
+class BrowserHistory(object):
+    def __init__(self, homepage: str):
+        self.li: list = [homepage] 
+        self.index: int = 0
+
+    def visit(self, url: str):
+        if self.index == len(self.li):
+            self.li.append(url)
+        
+        
+        self.index += 1
+
+    def back(self, steps: int):
+        if self.index - steps < 0:
+            pass
+
+    def forward(self, steps: int):
+        pass
